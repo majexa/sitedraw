de5bb2b3cba318e2cdcdef02d0f2a19e7fb5a0ae
masted <masted311@gmail.com>
2014-03-23 14:51:31 +0400

    ...

--- site/init.php
+++ site/init.php
@@ -2,5 +2,5 @@
 
 define('OUTPUT_DISABLE', true);
 Lib::addFolder(dirname(NGN_PATH).'/pm/lib');
-Ngn::addBasePath(dirname(NGN_PATH).'/projects/sitedrawhome/site');
-Ngn::addBasePath(dirname(dirname(NGN_PATH)).'/sd');
+//Ngn::addBasePath(dirname(NGN_PATH).'/projects/sitedrawhome/site');
+Ngn::addBasePath(dirname(NGN_PATH).'/sd');
--- site/lib/CtrlSitedrawDefault.class.php
+++ site/lib/CtrlSitedrawDefault.class.php
@@ -4,11 +4,14 @@ class CtrlSitedrawDefault extends CtrlCommon {
 use SdDefaultCtrl, HomeCtrl;
 
   function action_default() {
+    /*
     try {
       $html = file_get_contents('http://sitedrawhome.sitedraw.ru/index.html');
     } catch (Exception $e) {
       $html = Tt()->getTpl('auth/login');
     }
+    */
+    $html = Tt()->getTpl('auth/login');
     $html = str_replace('</head>', "<script src=\"/m/js/design.js\"></script>\n</head>", $html);
     $this->processExportedHtml($html);
   }
